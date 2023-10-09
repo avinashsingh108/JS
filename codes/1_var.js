c661fae7e7afdd0afaa253ff5b5595a694826b72
avinashsingh108 <137880543+avinashsingh108@users.noreply.github.com>
2023-10-09 17:42:33 +0000

    var_datatype

---
+++ codes/1_var.js
@@ -0,0 +1,6 @@
+const id = 14; //semicolon optional
+let name = "avinash"
+var mail = "as@gmail.com" //dont use var, scope problem
+state = "up" //not preferred
+let city; //undefined
+console.table([id,name,mail,state,city])
