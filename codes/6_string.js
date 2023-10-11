5298ffb40c5b5266d8b319068b0f8c14ee3ef101
avinashsingh108 <avinash.singh.24646@gmail.com>
2023-10-11 12:09:59 +0530

    string

---
+++ codes/6_string.js
@@ -0,0 +1,21 @@
+let name = "Avinash Singh"
+const num = 60
+
+console.log(`my name is ${name} and my package is ${num} lpa`);
+
+let naam = new String('Avinash Singh')
+console.log(`${naam}`);
+console.log(naam[10]);
+console.log(naam.toUpperCase());
+console.log(naam.charAt(5));
+console.log(naam.indexOf('h'));
+console.log(naam.substring(0,4))
+console.log(naam.slice(0,4));
+
+let state = "            Uttar Pradesh"
+console.log(state.trim());
+console.log(state.trim().replace(' ','50'));
+
+console.log(naam.includes('Avi'));
+
+console.log(naam.split(' ',3));
